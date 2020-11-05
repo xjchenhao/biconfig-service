803ccd75782bf850ebea9a6208ab60004d06d2d7
xjchenhao <wy.chenhao@qq.com>
2020-11-05 14:03:17 +0800

    增加数据源相关api

--- app/router.js
+++ app/router.js
@@ -7,6 +7,13 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
 
+  /* 数据源相关 */
+  router.get('/api/demo/dataSource/column', controller.dataSource.column); // 柱形图数据
+  router.get('/api/demo/dataSource/line', controller.dataSource.line); // 折线图数据
+  router.get('/api/demo/dataSource/bar', controller.dataSource.bar); // 条形图数据
+  router.get('/api/demo/dataSource/pie', controller.dataSource.pie); // 饼状图数据
+
+  /* 图表配置相关 */
   router.get('/api/graph/list', controller.graph.list); // 图表记录列表
   // router.post('/api/graph/create', controller.graph.create); // 创建新的图表
   // router.post('/api/graph/delete', controller.graph.delete); // 删除图表
