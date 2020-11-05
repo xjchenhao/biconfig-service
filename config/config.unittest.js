ae30384556ec95b841e8443f1e1ac7980fe5e498
xjchenhao <wy.chenhao@qq.com>
2020-11-05 10:28:24 +0800

    增加docker-compose和mongodb的支持

---
+++ config/config.unittest.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = {
+
+    mongoose: {
+      clients: {
+        app: {
+          url: 'mongodb://127.0.0.1:27017/biconfig',
+          options: {},
+        },
+      },
+    },
+  };
+
+  // add your user config here
+  const userConfig = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1550823333689_6157';
+
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
--- config/plugin.js
+++ config/plugin.js
@@ -2,8 +2,8 @@
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+  mongoose: {
+    enable: true,
+    package: 'egg-mongoose',
+  },
 };
