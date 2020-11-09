20f6f958280499cd9595fc509fd31411e13128a8
xjchenhao <wy.chenhao@qq.com>
2020-11-09 15:36:59 +0800

    增加初始化脚本

---
+++ init.d/mongo/createGraphRecord.js
@@ -0,0 +1,43 @@
+/*eslint-disable*/
+
+db.graphs.insert([
+    {
+        "_id" : ObjectId("5d8ad6e2a9ed49004c33bd41"),
+        "name" : "我是初始图表记录1",
+        "type": "Bar",
+        "apiUrl": "https://www.yuque.com/xjchenhao",
+        "attr": {
+            "xField": "xField",
+            "yField": "yField",
+        },
+        "titleShowType": 1,
+        "createTime" : ISODate('2020-11-11T02:10:10.000Z'),
+        "updateTime" : ISODate('2020-11-11T02:10:10.000Z'),
+    },
+    {
+        "_id" : ObjectId("5d8ad6e2a9ed49004c33bd42"),
+        "name" : '我是初始图表记录2',
+        "type": "Bar",
+        "apiUrl": "https://www.yuque.com/xjchenhao",
+        "attr": {
+            "xField": "xField",
+            "yField": "yField",
+        },
+        "titleShowType": 1,
+        "createTime" : ISODate('2020-11-11T02:10:10.000Z'),
+        "updateTime" : ISODate('2020-11-11T02:10:10.000Z'),
+    },
+    {
+        "_id" : ObjectId("5d8ad6e2a9ed49004c33bd43"),
+        "name" : '我是初始图表记录3',
+        "type": "Bar",
+        "apiUrl": "https://www.yuque.com/xjchenhao",
+        "attr": {
+            "xField": "xField",
+            "yField": "yField",
+        },
+        "titleShowType": 1,
+        "createTime" : ISODate('2020-11-11T02:10:10.000Z'),
+        "updateTime" : ISODate('2020-11-11T02:10:10.000Z'),
+    }
+]);
